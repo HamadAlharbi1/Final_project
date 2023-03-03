6420a6def8a67c4204090d59a4c86573594b2dd4
HamadAlharbi1 <hamad11131@gmail.com>
2023-03-03 21:24:26 +0300

    The project is linked with firebase auth,cloud

--- lib/App.dart
+++ lib/App.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:final_project/contents/modols.dart';
 import 'package:final_project/pages/Home_Page.dart';
 import 'package:flutter/material.dart';
 
@@ -10,6 +12,12 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   @override
+  void initState() {
+  
+
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:final_project/App.dart';
+import 'package:final_project/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
-  // WidgetsFlutterBinding.ensureInitialized();
-  // await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
