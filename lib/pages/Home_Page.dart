291497113b2e4be7450557a2e8027e9fae56adb6
HamadAlharbi1 <hamad11131@gmail.com>
2023-03-06 15:37:31 +0300

    1

--- lib/pages/Home_Page.dart
+++ lib/pages/Home_Page.dart
@@ -1,11 +1,14 @@
 import 'dart:async';
+import 'dart:ui';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:final_project/combonents/Constants/Qumash_Details_modols.dart';
 import 'package:final_project/combonents/Constants/Tailor_Details_modols.dart';
+import 'package:final_project/combonents/Constants/constants.dart';
 import 'package:final_project/combonents/Qumash_Card.dart';
 import 'package:final_project/combonents/Tailor_Card.dart';
 import 'package:final_project/pages/Tailor_Details_Page.dart';
+import 'package:final_project/pages/fill_new_maqas.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -57,6 +60,45 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBodyBehindAppBar: true,
+      backgroundColor: const Color(0xffeaeaea),
+      appBar: PreferredSize(
+        preferredSize: Size(MediaQuery.of(context).padding.top, 56),
+        child: ClipRRect(
+          child: BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 1, sigmaY: 10),
+            child: AppBar(
+              leading: InkWell(
+                  onTap: () {
+                    Navigator.push(context, MaterialPageRoute(builder: (context) {
+                      return const fillNewMaqas();
+                    }));
+                  },
+                  child: const Icon(Icons.add)),
+              automaticallyImplyLeading: false,
+              actions: [
+                InkWell(
+                  onTap: () => Navigator.pop(context),
+                  child: const Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Icon(
+                      Icons.arrow_forward_ios,
+                      color: Colors_and_Dimentions.icon_color,
+                    ),
+                  ),
+                )
+              ],
+              elevation: 0,
+              backgroundColor: const Color.fromARGB(0, 0, 0, 0).withOpacity(0.1),
+              centerTitle: true,
+              title: Image.network(
+                'https://cdn.discordapp.com/attachments/1081328393364189276/1082219855991803984/image_146.png',
+                fit: BoxFit.contain,
+              ),
+            ),
+          ),
+        ),
+      ),
       body: Padding(
         padding: const EdgeInsets.all(12),
         child: SizedBox(
@@ -75,7 +117,6 @@ class _HomePageState extends State<HomePage> {
                     item: i,
                   ),
                 ),
-              for (var qumash in qumashs) Qumash_Card(qumash: qumash)
             ],
           ),
         ),
