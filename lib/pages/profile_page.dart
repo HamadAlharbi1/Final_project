06f5a49736390a8e052604681ed46ab1ff01aebe
masubaie <subaie.mx@gmail.com>
2023-03-07 16:09:26 +0300

    updateHomeDrawerButton

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -61,172 +61,189 @@ class _ProfilePageState extends State<ProfilePage> {
       ),
       //drawer
       endDrawer: const DrawerWidget(),
-      body: Center(
-        child: Container(
-          margin: const EdgeInsets.all(24),
-          width: double.infinity,
-          decoration: BoxDecoration(
-            boxShadow: const [BoxShadow(color: Colors.grey, blurRadius: 0.6, spreadRadius: 0.8)],
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(12),
-          ),
-          child: Padding(
+      body: ListView(
+        children: [
+          Padding(
             padding: const EdgeInsets.all(8.0),
-            child: SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  const SizedBox(
-                    height: 12,
-                  ),
-                  const CircleAvatar(
-                    maxRadius: 55,
-                    backgroundColor: Colors_and_Dimentions.fontcolor,
-                    child: CircleAvatar(
-                      backgroundImage: NetworkImage(
-                          'https://t3.ftcdn.net/jpg/03/39/45/96/360_F_339459697_XAFacNQmwnvJRqe1Fe9VOptPWMUxlZP8.jpg'),
-                      radius: 50,
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 4,
-                  ),
-                  Text(
-                    user_name == null ? '' : user_name!,
-                    style: const TextStyle(
-                        fontSize: 28, fontWeight: FontWeight.w400, color: Colors_and_Dimentions.font_color),
-                  ),
-                  const SizedBox(
-                    height: 4,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: const [
-                      SizedBox(
-                        width: 4,
-                      ),
-                      SizedBox(
-                        width: 4,
-                      ),
-                    ],
-                  ),
-                  const Divider(
-                    thickness: 2,
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      const SizedBox(
-                        height: 12,
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 8),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.end,
+            child: Center(
+              child: Container(
+                margin: const EdgeInsets.all(24),
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  boxShadow: const [BoxShadow(color: Colors.grey, blurRadius: 0.6, spreadRadius: 0.8)],
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: SingleChildScrollView(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        const SizedBox(
+                          height: 12,
+                        ),
+                        const CircleAvatar(
+                          maxRadius: 55,
+                          backgroundColor: Colors_and_Dimentions.fontcolor,
+                          child: CircleAvatar(
+                            backgroundImage: NetworkImage(
+                                'https://t3.ftcdn.net/jpg/03/39/45/96/360_F_339459697_XAFacNQmwnvJRqe1Fe9VOptPWMUxlZP8.jpg'),
+                            radius: 50,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 4,
+                        ),
+                        Text(
+                          user_name == null ? '' : user_name!,
+                          style: const TextStyle(
+                              fontSize: 28, fontWeight: FontWeight.w400, color: Colors_and_Dimentions.font_color),
+                        ),
+                        const SizedBox(
+                          height: 4,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
                           children: const [
-                            Text(
-                              'بيانات التواصل ',
-                              style: TextStyle(
-                                  fontSize: 26, fontWeight: FontWeight.bold, color: Colors_and_Dimentions.font_color),
+                            SizedBox(
+                              width: 4,
+                            ),
+                            SizedBox(
+                              width: 4,
                             ),
                           ],
                         ),
-                      ),
-                      const SizedBox(
-                        height: 12,
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.only(right: 8),
-                            child: Text(
-                              user_email == null ? '' : '$user_email',
-                              style: const TextStyle(
-                                  fontSize: 20, fontWeight: FontWeight.w300, color: Colors_and_Dimentions.font_color),
-                            ),
-                          ),
-                          const Padding(
-                            padding: EdgeInsets.only(right: 8),
-                            child: Text(
-                              'البريد الإلكتروني',
-                              style: TextStyle(
-                                  fontSize: 20, fontWeight: FontWeight.w600, color: Colors_and_Dimentions.font_color),
-                            ),
-                          ),
-                        ],
-                      ),
-                      const SizedBox(
-                        height: 12,
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.only(right: 8),
-                            child: Text(
-                              user_phoneNumber == null ? '' : '$user_phoneNumber',
-                              style: const TextStyle(
-                                  fontSize: 20, fontWeight: FontWeight.w300, color: Colors_and_Dimentions.font_color),
-                            ),
-                          ),
-                          const Padding(
-                            padding: EdgeInsets.only(right: 8),
-                            child: Text(
-                              'رقم الجوال',
-                              style: TextStyle(
-                                  fontSize: 20, fontWeight: FontWeight.w600, color: Colors_and_Dimentions.font_color),
-                            ),
-                          ),
-                        ],
-                      ),
-                      const SizedBox(
-                        height: 24,
-                      ),
-                      const SizedBox(
-                        height: 32,
-                      ),
-                      InkWell(
-                        onTap: () {
-                          setState(() {
-                            FirebaseAuth.instance.signOut();
-                            Navigator.of(context).push(MaterialPageRoute(builder: (context) {
-                              return const LoginPage();
-                            }));
-                          });
-                        },
-                        child: Container(
-                          margin: const EdgeInsets.symmetric(horizontal: 36),
-                          alignment: Alignment.center,
-                          height: 55,
-                          width: double.infinity,
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(34),
-                              gradient: const LinearGradient(
-                                  begin: Alignment.centerLeft,
-                                  end: Alignment.centerRight,
-                                  colors: [Color.fromARGB(255, 194, 0, 32), Color.fromARGB(255, 243, 56, 56)])),
-                          child: const Text(
-                            'تسجيل الخروج',
-                            style: TextStyle(
-                              fontFamily: 'JosefinSans',
-                              fontSize: 24,
-                              fontWeight: FontWeight.normal,
-                              color: Colors.white,
-                            ),
-                          ),
+                        const Divider(
+                          thickness: 2,
                         ),
-                      ),
-                      const SizedBox(
-                        height: 32,
-                      )
-                    ],
-                  )
-                ],
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: [
+                            const SizedBox(
+                              height: 12,
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(left: 8),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: const [
+                                  Text(
+                                    'بيانات التواصل ',
+                                    style: TextStyle(
+                                        fontSize: 26,
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors_and_Dimentions.font_color),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 12,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.only(right: 8),
+                                  child: Text(
+                                    user_email == null ? '' : '$user_email',
+                                    style: const TextStyle(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.w300,
+                                        color: Colors_and_Dimentions.font_color),
+                                  ),
+                                ),
+                                const Padding(
+                                  padding: EdgeInsets.only(right: 8),
+                                  child: Text(
+                                    'البريد الإلكتروني',
+                                    style: TextStyle(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.w600,
+                                        color: Colors_and_Dimentions.font_color),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            const SizedBox(
+                              height: 12,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.only(right: 8),
+                                  child: Text(
+                                    user_phoneNumber == null ? '' : '$user_phoneNumber',
+                                    style: const TextStyle(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.w300,
+                                        color: Colors_and_Dimentions.font_color),
+                                  ),
+                                ),
+                                const Padding(
+                                  padding: EdgeInsets.only(right: 8),
+                                  child: Text(
+                                    'رقم الجوال',
+                                    style: TextStyle(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.w600,
+                                        color: Colors_and_Dimentions.font_color),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            const SizedBox(
+                              height: 24,
+                            ),
+                            const SizedBox(
+                              height: 32,
+                            ),
+                            InkWell(
+                              onTap: () {
+                                setState(() {
+                                  FirebaseAuth.instance.signOut();
+                                  Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+                                    return const LoginPage();
+                                  }));
+                                });
+                              },
+                              child: Container(
+                                margin: const EdgeInsets.symmetric(horizontal: 36),
+                                alignment: Alignment.center,
+                                height: 55,
+                                width: double.infinity,
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(34),
+                                    gradient: const LinearGradient(
+                                        begin: Alignment.centerLeft,
+                                        end: Alignment.centerRight,
+                                        colors: [Color.fromARGB(255, 194, 0, 32), Color.fromARGB(255, 243, 56, 56)])),
+                                child: const Text(
+                                  'تسجيل الخروج',
+                                  style: TextStyle(
+                                    fontFamily: 'JosefinSans',
+                                    fontSize: 24,
+                                    fontWeight: FontWeight.normal,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 32,
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                ),
               ),
             ),
           ),
-        ),
+        ],
       ),
     );
   }
