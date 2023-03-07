744fc009512ab2a89ad88bde475cd9599591c3c9
HamadAlharbi1 <hamad11131@gmail.com>
2023-03-07 23:52:14 +0300

    after merege

--- lib/App.dart
+++ lib/App.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:final_project/combonents/Constants/Qumash_Details_modols.dart';
 import 'package:final_project/combonents/Constants/Tailor_Details_modols.dart';
+import 'package:final_project/pages/Home_Page.dart';
 
 import 'package:flutter/material.dart';
 
@@ -30,7 +31,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-
+      home: HomePage(),
       debugShowCheckedModeBanner: false,
     );
   }
